a674004e470a481d18e2df17112fa2332edaf420
pleget_JH <125358006+PLZGET@users.noreply.github.com>
2024-02-05 09:25:32 +0900

    [Bronze II] Title: 시험 감독, Time: 528 ms, Memory: 144340 KB -BaekjoonHub

--- 백준/Bronze/13458. 시험 감독/시험 감독.py
+++ 백준/Bronze/13458. 시험 감독/시험 감독.py
@@ -1,25 +1,39 @@
-import math
-
-def calculate_supervisors(N, A, B, C):
+def min_supervisors(test_rooms, examinees, B, C):
     total_supervisors = 0
 
-    for i in range(N):
-        # 총감독관이 감독 가능한 인원만큼 감독
-        A[i] -= B
+    for i in range(test_rooms):
+        # 각 시험장의 응시자 수
+        students = examinees[i]
+
+        # 총감독관이 감시할 수 있는 응시자 수를 뺀 나머지 응시자 수
+        remaining_students = students - B
+
+        # 총감독관은 무조건 1명 필요
         total_supervisors += 1
 
-        # 남은 응시자가 있을 경우 부감독관이 감독
-        if A[i] > 0:
-            total_supervisors += math.ceil(A[i] / C)
+        # 남은 응시자 수가 양수일 경우 부감독관이 필요
+        if remaining_students > 0:
+            # 부감독관이 감시할 수 있는 응시자 수로 나누어 필요한 부감독관 수 계산
+            # 7//4 = 1 이니까 +1 해서 부감독관 수 맞쳐줌
+            if(remaining_students % C == 0):
+                additional_supervisors = (remaining_students // C)
+            else:
+                additional_supervisors = (remaining_students // C) + 1
+            
+            total_supervisors += additional_supervisors
 
     return total_supervisors
 
-# 입력 받기
-N = int(input())  # 시험장의 개수
-A = list(map(int, input().split()))  # 각 시험장의 응시자 수
-B, C = map(int, input().split())  # 총감독관과 부감독관이 감시 가능한 응시자 수
 
-result = calculate_supervisors(N, A, B, C)
+# 시험장 개수(N) 입력
+test_rooms = int(input())
+
+# 각 시험장에 있는 응시자수 Ai(1<=Ai<=1,000,000)
+examinees = list(map(int, input().split()))
+
+# 총감독관, 부감독관 감시가능한 응시자수 B, C
+B, C = map(int, input().split())
 
 # 결과 출력
+result = min_supervisors(test_rooms, examinees, B, C)
 print(result)
